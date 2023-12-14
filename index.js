ae637d5c4c35fe6a14d5d52643a2cd2a79658dbd
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2023-12-14 14:18:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log('multiply:', multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log('random:', random);
+
+const num3 = 24;  
+const num4 = 5;
+const mod = num3 % num4;
+console.log('mod:', mod);
+
+const max = Math.max(10, 20, 15);
+console.log('max:', max);
